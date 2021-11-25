8008ed63ac5ca724766eda533099cd92d19eb650
Anadyrskie <anadyrskie@anadyr.us>
2021-11-25 00:38:57 -0700

    update configTemplate.py

--- configTemplate.py
+++ configTemplate.py
@@ -6,14 +6,14 @@ solar_forecast = {
     "declination" : "",
     "azimuth" : "",
     "kWpanels" : "",
-    # "api_function":"watthours/day/",
-    "watts_required" : 0
+    # "api_function":"watthours/day/",  #currently unused, see url concatenation
+    "watts_required" : 0  # use Int
 }
 # <editor-fold desc="url concatenation">
 solar_forecast["url"] = ("https://api.forecast.solar/" +
                          solar_forecast["api_key"] +
                          "/estimate/" +
-                         # solar_forecast["api_function"] +
+                         # solar_forecast["api_function"] +  # parse_JSON needs rewrite for this to work
                          solar_forecast["lat"] + "/" +
                          solar_forecast["long"] + "/" +
                          solar_forecast["declination"] + "/" +
@@ -25,7 +25,7 @@ solar_forecast["url"] = ("https://api.forecast.solar/" +
 twilio_config = {
     "account_sid":"ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     "auth_token":"your_auth_token",
-    "from":"+15017250604",  #text is sent from
+    "from":"+15017250604",  #text is sent from this number
     "to": [
     "+15558675309",         #recipient 1
     "+12223331234"          #recipient 2
